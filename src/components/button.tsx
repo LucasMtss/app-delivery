5b7f0102b7e1485b59cc027b8f0cef14e7cafc1b
Lucas Matos <54159333+LucasMtss@users.noreply.github.com>
2024-03-13 08:08:15 -0300

    Revert "template de doceria"

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ interface IButtonProps {
 
 export function Button({title, transparent, onClick, isLoading}: IButtonProps){
     return (
-        <button disabled={isLoading} onClick={onClick} className={`min-w-[200px] text-center font-bold px-4 py-2 rounded-md text-lg ${transparent ? 'bg-transparent text-red-400 border border-red-400' : 'bg-red-400 text-slate-50'}`}>
+        <button disabled={isLoading} onClick={onClick} className={`min-w-[200px] text-center font-bold px-4 py-2 rounded-md text-lg ${transparent ? 'bg-transparent text-lime-400 border border-lime-400' : 'bg-lime-400 text-slate-700'}`}>
             {isLoading ? 'Carregando...' : title}
         </button>
     )
